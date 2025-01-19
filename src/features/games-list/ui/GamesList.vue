9952188c68510a712853a7eba52df96a5d268e66
evgeny <afazhenya2001@gmail.com>
2025-01-19 17:51:21 +0300

    games list

--- src/features/games-list/ui/GamesList.vue
+++ src/features/games-list/ui/GamesList.vue
@@ -1,7 +1,18 @@
 <template>
-    <div class="text-red-200">
-      Game
-    </div>
+  <h1 class="m-2">
+    Games:
+  </h1>
+  <div class="flex gap-x-8 m-2">
+    <Card v-for="game of gamesList" :key="game.id" class="w-[350px]">
+      <CardHeader>
+        <CardTitle> {{ game.id }}</CardTitle>
+        <CardDescription>Created at: {{ game.createdAt }}</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <BaseButton>Join</BaseButton>
+      </CardContent>
+    </Card>
+  </div>
 </template>
 <script setup lang="ts">
 import { getGamesByStatus } from '~/entities/game';
