f9b571b1ce803077b190cf7bf5199890256bff5a
evgeny <afazhenya2001@gmail.com>
2025-02-01 23:45:51 +0300

    auth template

---
+++ src/app/auth/index.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import AuthForm from '~/features/auth/ui/AuthForm.vue';
+
+</script>
+
+<template>
+    <AuthForm />
+</template>
---
+++ src/features/auth/ui/AuthForm.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="v-full h-screen flex items-center justify-center">
+        <div class="w-96">
+            <div class="grid w-full items-center gap-1.5">
+                <Label for="login">Login</Label>
+                <Input id="login" type="text" />
+            </div>
+            <div class="mt-4 grid w-full items-center gap-1.5">
+                <Label for="password">Pasword</Label>
+                <Input id="password" type="password" />
+            </div>
+        </div>
+    </div>
+</template>
+<script setup lang="ts">   
+</script>
