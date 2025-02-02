215ade8c935b0368b7b4dccfb5091747b827e103
evgeny <afazhenya2001@gmail.com>
2025-02-02 17:18:01 +0300

    fixes

--- src/features/auth/ui/AuthForm.vue
+++ src/features/auth/ui/AuthForm.vue
@@ -1,16 +1,23 @@
 <template>
+    <!-- TODO check shadcn styles -->
     <div class="v-full h-screen flex items-center justify-center">
         <div class="w-96">
-            <div class="grid w-full items-center gap-1.5">
-                <Label for="login">Login</Label>
-                <Input id="login" type="text" />
+            <div class="relative w-full max-w-sm items-center">
+                <Input id="login" type="text" placeholder="login" class="pl-10" />
+                <span class="absolute start-0 inset-y-0 flex items-center justify-center px-2">
+                <PersonIcon class="size-6 text-muted-foreground" />
+                </span>
             </div>
-            <div class="mt-4 grid w-full items-center gap-1.5">
-                <Label for="password">Pasword</Label>
-                <Input id="password" type="password" />
+            <div class="mt-4 relative w-full max-w-sm items-center">
+                <Input id="password" type="password" placeholder="password" class="pl-10" />
+                <span class="absolute start-0 inset-y-0 flex items-center justify-center px-2">
+                <LockClosedIcon class="size-6 text-muted-foreground" />
+                </span>
             </div>
         </div>
     </div>
 </template>
 <script setup lang="ts">   
+import { PersonIcon } from '@radix-icons/vue'
+import { LockClosedIcon } from '@radix-icons/vue'
 </script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,17 +6,8 @@ module.exports = {
   safelist: ["dark"],
   content: [
     './src/**/*.{vue,js,ts}',
-    './shared/**/*.{vue,js,ts}',
-    './app/**/*.{vue,js,ts}',
   ],
   theme: {
-    container: {
-      center: true,
-      padding: "2rem",
-      screens: {
-        "2xl": "1400px",
-      },
-    },
     extend: {
       colors: {
         border: "hsl(var(--border))",
@@ -54,34 +45,9 @@ module.exports = {
         },
       },
       borderRadius: {
-        xl: "calc(var(--radius) + 4px)",
-        lg: "var(--radius)",
-        md: "calc(var(--radius) - 2px)",
+        lg: `var(--radius)`,
+        md: `calc(var(--radius) - 2px)`,
         sm: "calc(var(--radius) - 4px)",
-      },
-      keyframes: {
-        "accordion-down": {
-          from: { height: 0 },
-          to: { height: "var(--radix-accordion-content-height)" },
-        },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: 0 },
-        },
-        "collapsible-down": {
-          from: { height: 0 },
-          to: { height: 'var(--radix-collapsible-content-height)' },
-        },
-        "collapsible-up": {
-          from: { height: 'var(--radix-collapsible-content-height)' },
-          to: { height: 0 },
-        },
-      },
-      animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
-        "collapsible-down": "collapsible-down 0.2s ease-in-out",
-        "collapsible-up": "collapsible-up 0.2s ease-in-out",
       },
     },
   },
